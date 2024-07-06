eb14be39898750d6684bcfd46dc8bd3d39d947a6
Aditi Srinivas <aditisrinivas@Aditis-MacBook-Air.local>
2024-07-06 13:32:32 +0530

    Final features added to the project

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -12,20 +12,25 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+import { fetchCardData } from '@/app/lib/data';
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
 import {
   CheckIcon,
   ClockIcon,
